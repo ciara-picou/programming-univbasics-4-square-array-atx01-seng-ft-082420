c684cebde395cfd557e8790c7ed76d290552bafa
Ciara Picou <ciarapicou@att.net>
2020-07-20 18:15:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -24,6 +24,16 @@
 # return new_array
 # end
 
+def square_array(array)
+  counter = 0
+new_array = []
+while array[counter] do
+new_array.push(array[counter] * array[counter])
+  counter += 1
+end
+return new_array
+end
+
 # def square_array(array)
 # new_array = []
 # array.length.times { |index|
