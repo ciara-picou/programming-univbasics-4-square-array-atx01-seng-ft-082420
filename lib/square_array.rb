18c0bb34e41a4bf424d70bc1e5d82e913c65345f
Ciara Picou <ciarapicou@att.net>
2020-07-20 18:19:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -28,7 +28,7 @@ def square_array(array)
   counter = 0
 new_array = []
 until !array[counter] do
-new_array >> (array[counter] * array[counter])
+new_array << (array[counter] * array[counter])
   counter += 1
 end
 return new_array
